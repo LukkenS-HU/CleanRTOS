9f8734490ae97485c13cecdcaf33da04b036482d
LukkenS <stijn.lukken@student.hu.nl>
2022-11-09 12:20:12 +0100

    Add crt_HWTimer, an ESP hardware timer wrapper

---
+++ src/crt_HWTimer.h
@@ -0,0 +1,81 @@
+//
+// Created by lukkens on 11/9/22.
+//
+// This file is licensed under the GNU GPLv3 only.
+
+#ifndef CRT_HWTIMER_H
+#define CRT_HWTIMER_H
+
+#include "crt_Waitable.h"
+#include "crt_Task.h"
+#include <driver/gptimer.h>
+
+#include <functional>
+#include <utility>
+
+namespace crt
+{
+    class HWTimer
+    {
+    public:
+        typedef std::function<bool(Task *, const gptimer_alarm_event_data_t *)> Callback_t;
+
+        explicit HWTimer(Callback_t callback, Task *task, uint64_t intervalUs)
+        : _callback(std::move(callback)), _task(task), _timerHandle()
+        {
+            gptimer_config_t configuration = {
+                    .clk_src = GPTIMER_CLK_SRC_DEFAULT,
+                    .direction = GPTIMER_COUNT_UP,
+                    .resolution_hz = 1000'000, // 1 MHz -> 1 uS resolution
+            };
+
+            gptimer_new_timer(&configuration, &_timerHandle);
+
+            gptimer_event_callbacks_t callbackConfig = {
+                    .on_alarm = &TimerCallback
+            };
+            gptimer_register_event_callbacks(_timerHandle, &callbackConfig, this);
+
+            gptimer_alarm_config_t alarmConfig = {
+                    .alarm_count = intervalUs,
+                    .reload_count = 0xFFFFFFFFFFFFFFFF,
+                    .flags = {
+                            .auto_reload_on_alarm = true
+                    }
+            };
+            gptimer_set_alarm_action(_timerHandle, &alarmConfig);
+
+            gptimer_enable(_timerHandle);
+        }
+
+        ~HWTimer()
+        {
+            gptimer_stop(_timerHandle);
+            gptimer_del_timer(_timerHandle);
+        }
+
+        void Start()
+        {
+            gptimer_start(_timerHandle);
+        }
+
+        void Stop()
+        {
+            gptimer_stop(_timerHandle);
+        }
+
+    private:
+        static bool TimerCallback(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_ctx)
+        {
+            auto *self = (HWTimer*)user_ctx;
+            return self->_callback(self->_task, edata);
+        }
+
+        Callback_t _callback;
+        Task *_task;
+
+        gptimer_handle_t _timerHandle;
+    };
+}
+
+#endif //CRT_HWTIMER_H
